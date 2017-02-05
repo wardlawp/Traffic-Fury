47af15851269fb06579e51d49f0476d7acff1a88
Philip Wardlaw <wardlaw@ualberta.ca>
2017-02-05 14:58:12 -0700

    Scheduling WIP

---
+++ Assets/Scripts/TrafficController.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Assets.Scripts.TrafficController;
+
+
+public class Tuple<T1, T2>
+{
+    public T1 key { get; private set; }
+    public T2 value { get; private set; }
+
+    internal Tuple(T1 key, T2 value)
+    {
+        this.key = key;
+        this.value = value;
+    }
+}
+
+public class TrafficController : MonoBehaviour {
+
+    public GameObject[] carProtoTypes;
+    public GameObject background;
+
+    public float leftLaneX;
+    public float rightLaneX;
+    public int numLanes;
+
+    private TrafficQue que;
+    private List<Tuple<GameObject, TrafficScheduleEntry>> runningCars;
+
+	// Use this for initialization
+	void Start () {
+        runningCars = new List<Tuple<GameObject, TrafficScheduleEntry>>();
+
+        buildQue();
+        //Debug.Log(que.lenght);
+        //Debug.Log(que.getCurrentsEntries(5f)[0].lane);
+        //Debug.Log(que.lenght);
+    }
+
+    
+
+
+    // Update is called once per frame
+    void Update () {
+        foreach(TrafficScheduleEntry e in que.getCurrentsEntries(Time.time))
+        {
+            createCar(e);
+        }
+
+        foreach(GameObject car in cars)
+        {
+            runCar(car);
+        }
+
+        Debug.Log(que.lenght);
+        //Todo avoid collisions
+
+    }
+
+    void createCar(TrafficScheduleEntry e)
+    {
+        Vector3 position = calculatePosition(e.lane, e.appearAtBottom());
+        Instantiate(cars[e.carType], position, new Quaternion());
+    }
+    void runCar(GameObject car)
+    {
+        
+    }
+
+    private void buildQue()
+    {
+        que = new TrafficQue();
+
+        que.addEntry(
+            TrafficEntryBuilder.start()
+                .setLane(1)
+                .setCarType(1)
+                .appearAt(10f, 5f)
+                .accelerateAt(15f, 10f, 2f)
+                .get()
+            );
+
+        que.addEntry(
+            TrafficEntryBuilder.start()
+                .setLane(2)
+                .setCarType(2)
+                .appearAt(4f, 4f)
+                .get()
+            );
+    }
+}
---
+++ Assets/Scripts/TrafficController/TrafficSchedule.cs
@@ -0,0 +1,216 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace Assets.Scripts.TrafficController
+{
+
+    public class TrafficQue
+    {
+        private List<TrafficScheduleEntry> list;
+
+        public int lenght { get { return list.Count; } }
+
+        public TrafficQue()
+        {
+            list = new List<TrafficScheduleEntry>();
+        }
+
+        public void addEntry(TrafficScheduleEntry newEntry)
+        {
+            bool entered = false;
+            foreach (TrafficScheduleEntry entry in list)
+            {
+                if(entry.appearAt() > newEntry.appearAt())
+                {
+                    entered = true;
+                    list.Insert(list.IndexOf(entry), newEntry);
+                    break;
+                }
+            }
+
+            if (!entered)
+            {
+                //Place at end
+                list.Add(newEntry);
+            }
+        }
+
+
+        public List<TrafficScheduleEntry> getCurrentsEntries(float timeNow)
+        {
+            List<TrafficScheduleEntry> result = new List<TrafficScheduleEntry>();
+            List<TrafficScheduleEntry> removeItems = new List<TrafficScheduleEntry>();
+
+            foreach (TrafficScheduleEntry entry in list)
+            {
+                if(entry.appearAt() < timeNow)
+                {
+                    result.Add(entry);
+                    removeItems.Add(entry);
+                }
+            }
+
+            foreach (TrafficScheduleEntry entryToBeRemoved in removeItems)
+                list.Remove(entryToBeRemoved);
+
+            return result;
+        }
+    }
+
+    public class BuilderException: Exception
+    {
+        public BuilderException (string message): base(message) {}
+    }
+
+
+    public class TrafficEntryBuilder
+    {
+        private TrafficScheduleEntry entry;
+
+        public static TrafficEntryBuilder start()
+        {
+            return new TrafficEntryBuilder();
+        }
+
+
+        public TrafficEntryBuilder()
+        {
+            entry = new TrafficScheduleEntry();
+        }
+
+        public TrafficEntryBuilder setLane(int lane)
+        {
+            entry.lane = lane;
+
+            return this;
+        }
+
+        public TrafficEntryBuilder setCarType(int carType)
+        {
+            entry.carType = carType;
+
+            return this;
+        }
+
+        public TrafficEntryBuilder appearAt(float time, float speed, bool appearAtBottom = true)
+        {
+            TrafficEvent e = new TrafficEvent();
+
+            e.type = TrafficEvent.types.Appear;
+            e.time = time;
+            e.speed = speed;
+            e.appearAtBottom = appearAtBottom;
+
+            entry.events.Add(e);
+
+            return this;
+        }
+
+        public TrafficEntryBuilder accelerateAt(float time, float newSpeed, float accelDuration)
+        {
+            TrafficEvent e = new TrafficEvent();
+
+            e.type = TrafficEvent.types.Accelerate;
+            e.time = time;
+            e.speed = newSpeed;
+            e.duration = accelDuration;
+
+            entry.events.Add(e);
+
+            return this;
+        }
+
+        public TrafficEntryBuilder explodeAt(float time)
+        {
+            TrafficEvent e = new TrafficEvent();
+
+            e.type = TrafficEvent.types.Explode;
+            e.time = time;
+
+            entry.events.Add(e);
+
+            return this;
+        }
+
+        public TrafficScheduleEntry get()
+        {
+            if (!entry.hasAppearance())
+            {
+                throw new BuilderException("TrafficScheduleEntry must have appear event");
+            }
+
+            return entry;
+        }
+
+    }
+
+
+    public class TrafficScheduleEntry
+    {
+        public TrafficScheduleEntry()
+        {
+            events = new List<TrafficEvent>();
+        }
+        
+        public int carType { get; set; }
+        public int lane { get; set; }
+
+        public List<TrafficEvent> events { get; set; }
+
+        public bool hasAppearance()
+        {
+            foreach (TrafficEvent e in events)
+            {
+                if (e.type == TrafficEvent.types.Appear)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public float appearAt()
+        {
+            float result = -1.0f;
+
+            foreach(TrafficEvent e in events)
+            {
+                if(e.type == TrafficEvent.types.Appear)
+                {
+                    result = e.time;
+                    break;
+                }
+            }
+
+            //Todo throw some sort of exception if we didn't have an Appear event
+            return result;
+        }
+
+        public bool appearAtBottom()
+        {
+            foreach (TrafficEvent e in events)
+            {
+                if (e.type == TrafficEvent.types.Appear)
+                {
+                    return e.appearAtBottom;
+                }
+            }
+
+            return false;
+        }
+
+
+    }
+
+    public class TrafficEvent
+    {
+        public enum types { Appear, Accelerate, Explode };
+        public types type;
+
+        public bool appearAtBottom;
+        public float time;
+        public float speed;
+        public float duration;
+    }
+}
